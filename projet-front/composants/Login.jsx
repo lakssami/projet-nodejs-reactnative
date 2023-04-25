f18847933519b2303711950d28d0ee51dcf0f517
lakssami <boumalaka@hotmail.com>
2023-04-25 15:39:09 +0200

    lien entre identifiant back et front

--- projet-front/composants/Login.jsx
+++ projet-front/composants/Login.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React , {useState} from 'react'
 
-const Login = () => {
+
+const Login = ({navigation}) => {
+    const [email, setEmail] =useState("zrrrt@yahoo.fr")
+    const [password, setPassword] =useState("Paris12345")
+
+    const submit = function(){
+        const identifiant = { email, password}
+        console.log(identifiant)
+        fetch("http://10.0.2.2:4003/login" , { method : "post" , body : JSON.stringify(identifiant), headers : {"content-type" : "application/json"}})
+        .then(reponse=> reponse.json())
+        .then(data=>console.log(data))
+    }
   return (
     <View>
       <Text>Login</Text>
+      <TextInput placeholder="email" style={styles.forme} value={email} onChangeText={(text) => setEmail(text)}/>
+      <TextInput placeholder='password' style={styles.forme} value={password} onChangeText={(text) => setPassword(text)}/>
+      <Button onPress={ submit } title="voulez-vous créer un compte?" />
     </View>
   )
 }
 
+
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    forme : {
+        borderWidth : 1,
+        padding : 15 ,
+        borderColor : "black",
+        backgroundColor : "white",
+        marginBottom : 15
+    }
+})
