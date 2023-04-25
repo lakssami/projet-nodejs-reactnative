4fc8ea5a46192c0d70f1a8427cfdcc79bb30b41e
lakssami <boumalaka@hotmail.com>
2023-04-25 14:22:24 +0200

    mise en place barre de menu front

--- projet-front/App.js
+++ projet-front/App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './composants/Login';
+import Accueil from './composants/Accueil'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator()
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="accueil" component={ Accueil }
+             options={{
+              tabBarIcon : ()=><MaterialCommunityIcons name="home" size={30} />
+             }}/>
+            <Tab.Screen name="connexion" component={ Login }
+            options={{
+              tabBarIcon : ()=><MaterialCommunityIcons name="login" size={30} />
+             }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +33,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ projet-front/composants/Accueil.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+const Accueil = () => {
+  return (
+    <View>
+      <Text>Ensemble des oeuvres disponible dans le musée</Text>
+    </View>
+  )
+}
+
+export default Accueil
+
+const styles = StyleSheet.create({})
